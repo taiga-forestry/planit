7aa1111ef70a693f462e171796f08c6c4d4b1bdd
Tiger Ji <tiger.j540@gmail.com>
2024-12-28 20:13:09 -0600

    build: add yarn script to generate supabase types

--- src/database/client.ts
+++ src/database/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-import { Database } from "./database.types";
+import { Database } from "./supabase.types";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
