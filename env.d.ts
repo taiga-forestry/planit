7d00f3662848f8548378043be8300dcc1d63fa2b
Tiger Ji <tiger.j540@gmail.com>
2024-12-29 18:06:14 -0600

    build: update supabase pull script and add VITE_BASE_URL

--- env.d.ts
+++ env.d.ts
@@ -2,6 +2,7 @@
 
 // see: https://v2.vitejs.dev/guide/env-and-mode.html#production-replacement
 interface ImportMetaEnv {
+  readonly VITE_BASE_URL: string;
   readonly VITE_GOOGLE_MAPS_API_KEY: string;
   readonly VITE_GOOGLE_MAPS_MAP_ID: string;
   readonly VITE_GOOGLE_PLACES_API_KEY: string;
